1cb8060d34548e1f5e52448d38af3212bb1df891
kirill0720 <knete-87@mail.ru>
2022-05-20 17:33:25 +0300

    use slug instead of int

--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
@@ -36,19 +36,27 @@ def login(request):
     return HttpResponse(f"<h1>Пустая страница</h1>")
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat,
+    }
+    return render(request, 'women/post.html', context=context)
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
     context = {
         'menu': menu,
         'title': 'Отображение по рубрикам страница',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
 
     return render(request, 'women/index.html', context=context)
