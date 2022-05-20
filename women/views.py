3c6f68a8f17a333df9d23b2286b097e942d05f6b
kirill0720 <knete-87@mail.ru>
2022-05-20 15:00:06 +0300

    posts and cats with tags simple and inclusive

--- women/views.py
+++ women/views.py
@@ -11,12 +11,7 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 
 def index(request):
-    posts = Women.objects.all()
-    cats = Category.objects.all()
-
     context = {
-        'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная страница',
         'cat_selected': 0,
@@ -50,15 +45,7 @@ def pageNotFound(request, exception):
 
 
 def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
-
-    if len(posts) == 0:
-        raise Http404()
-
     context = {
-        'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Отображение по рубрикам страница',
         'cat_selected': cat_id,
