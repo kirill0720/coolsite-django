0d41f8025ad319344438425d7c53b289f3d69587
kirill0720 <knete-87@mail.ru>
2022-05-20 22:16:12 +0300

    form to add page is added

--- women/views.py
+++ women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import *
 from .models import *
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
@@ -25,7 +26,19 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse(f"<h1>Пустая страница</h1>")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, "ошибка добавления поста")
+
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
