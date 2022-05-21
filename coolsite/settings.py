3d945e776a13ab982be4c8bd777ac6ca26252e67
kirill0720 <knete-87@mail.ru>
2022-05-21 14:03:14 +0300

    form connected with ORM used

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScrip`t, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
